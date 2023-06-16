49900e5b7b91117898ccdcf3cb69c8a3cffb9ed4
Vtigami <136007717+Vtigami@users.noreply.github.com>
2023-06-16 08:17:30 -0300

    Update dgram.h

--- dgram.h
+++ dgram.h
@@ -1,20 +1,50 @@
-#ifndef DGRAM_H
-#define DGRAM_H
+#define MAXPLAYERS 8
+#define MSGSIZE 32
+#define LINESIZE 16
+#define IPSIZE 16
+#define MARCINI 126
+#define MARCFIM 94
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+typedef enum
+{ 
+    DISTRIBCARDS= 1,
+    BASTAO,
+    JOGADA,             //primeiro caractere numero de cartas e segundo valor da carta
+    EMPTYHAND 
+    
+} type_t ;
 
-#define PORTA 2000
-#define MSGSIZE 1000
+typedef enum
+{ 
+    BACK= 0,
+    FRONT  
+    
+} reconnect_t ;
 
-int create_socket(struct sockaddr_in* saddr,char* ip);
 
-int main_dgram();
+typedef struct mensagem_t{
+    char origem;
+    char tipo;
+    char jogada[LINESIZE];
+    char cond_rec[MAXPLAYERS];
 
+}mensagem_t;
 
-#endif
+typedef struct info_t{
+    int numPlayers;
+    char idIps[MAXPLAYERS][IPSIZE];
+    
 
+}info_t;
+
+int create_socket(char* ip,int door);
+
+FILE* openFile(char* path,char* action);
+
+int getHostIp(char** host_ip);
+
+void replaceFirst(char* s,char c1, char c2);
+
+int getInitInfo(char *hostId,char *destId,info_t* info,int* door);
+
+struct sockaddr_in init_sockaddrin(info_t* info,char destId,int door);
