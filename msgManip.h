99691aee764647038a610620bfbf4bb33074c8a5
Vtigami <136007717+Vtigami@users.noreply.github.com>
2023-06-16 11:45:25 -0300

    Update msgManip.h

--- msgManip.h
+++ msgManip.h
@@ -8,8 +8,10 @@ void separateMessage(mensagem_t* m, char* buffer,int numPlayers);
 
 int verifyReceb(mensagem_t* m, int numPlayers);
 
-char* getCondRecDistrib(int id,int numPlayers);
+void getCondRecDistrib(int id,char *v);
 
-char* getCondRecBase(int id,int numPlayers);
+void getCondRecBase(int id,char* v);
 
 int recvMensagem(int s,char* buffer, int buffer_size,struct sockaddr_in* raddr,socklen_t len,char tipo,int numPlayers);
+
+int verifyMsg(char* buffer,int size);
